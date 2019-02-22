c399b2db59a569fd85af57cf770c7280e63186a7
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-22 16:46:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
 
   attr_accessor :total, :discount
-
+  @@items = []
   def initialize(discount = 0)
       @total = 0
       @discount = discount
@@ -21,7 +21,6 @@ class CashRegister
   end
 
   def items
-    @@items = []
     @@items
   end
 
